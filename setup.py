1b078055ea05b83581464e441a2b12b83a6a0470
kirby <devnull@localhost>
2006-03-30 15:42:28 +0000

    [project @ 2006-03-30 15:42:28 by kirby] update for 0.2.4 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="FIAT", \
-      version="0.2.2", \
+      version="0.2.4", \
       description="FInite element Automatic Tabulator", \
       author="Robert C. Kirby", \
       author_email="kirby@uchicago.edu", \
