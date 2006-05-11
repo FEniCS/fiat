9a7f04bc2f22233e84dc203c95750002ed36bb4c
kirby <devnull@localhost>
2006-05-11 14:33:33 +0000

    [project @ 2006-05-11 14:33:33 by kirby] updated doc string in __init__.py, added some new things to Cools quadrature rules

--- FIAT/__init__.py
+++ FIAT/__init__.py
@@ -1,3 +1,3 @@
-"""Limited version of FIAT (FInite element Automatic Tabulator)
-that only supports constructing and evaluating Lagrange elements.
+"""FInite element Automatic Tabulator -- supports constructing and 
+evaluating arbitrary order Lagrange and many other elements.
 Simplices in one, two, and three dimensions are supported."""
