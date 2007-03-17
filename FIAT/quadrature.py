116e830435d3eee272aaae68e4ca088847895c49
knepley <knepley@knepley-laptop>
2007-03-17 10:11:01 -0500

    Quadrature should remember its degree

--- FIAT/quadrature.py
+++ FIAT/quadrature.py
@@ -67,6 +67,7 @@ class GaussQuadrature( JacobiQuadrature ):
     the weights both to be zero."""
     def __init__( self , m ):
         JacobiQuadrature.__init__( self , 0. , 0. , m )
+        self.degree = m;
         return
 
 class CollapsedQuadratureTriangle( QuadratureRule ):
@@ -79,6 +80,7 @@ class CollapsedQuadratureTriangle( QuadratureRule ):
         pts = map( expansions.xi_triangle, \
                    [ (x,y) for x in ptx for y in pty ] )
         QuadratureRule.__init__( self , pts , ws )
+        self.degree = m;
         return
 
 class CollapsedQuadratureTetrahedron( QuadratureRule ):
@@ -98,6 +100,7 @@ class CollapsedQuadratureTetrahedron( QuadratureRule ):
                      for y in pty \
                      for z in ptz ] )
         QuadratureRule.__init__( self , pts , ws )
+        self.degree = m;
         return
 
 def make_quadrature_triangle( m ):
