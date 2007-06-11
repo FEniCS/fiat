07703327177e1130952a86caae89a0b6c96c26da
Robert C. Kirby <kirby@uchicago.edu>
2007-06-11 17:02:02 +0200

    Fixed some minor bug in constrainedspaces.py edited some tests

--- FIAT/testRT.py
+++ FIAT/testRT.py
@@ -6,8 +6,20 @@ degree = 0
 U = RaviartThomas.RaviartThomas(shape,degree)
 
 #pts = shapes.make_points(shape,shape,0,degree+3)
+#pts = shapes.make_lattice( shape , 1 )
 
 #print pts
-#print U.tabulate( pts )
+#print U.function_space().tabulate( pts )
 
-print U.dual_basis().entity_ids
+# check outward normal
+for i in range(3):
+    pts = shapes.make_points( shape , 1 , i , degree+2 )
+    print pts
+    vals = U.function_space().tabulate( pts )
+    n = shapes.normals[shape][i]
+    print n
+    print vals[:,0,:]
+    print vals[:,1,:]
+    print vals[:,0,:]*n[0] + vals[:,1,:]*n[1]
+    print
+
