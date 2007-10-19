bd5061c46fe52f0c4c7c5a83f8405098c17a3bdc
kirby <kirby@localhost>
2007-10-19 12:43:03 -0500

    Bug fix in 3d dual space in Nedelec.py

--- FIAT/testned.py
+++ FIAT/testned.py
@@ -1,9 +1,10 @@
 import Nedelec, functional, numpy, polynomial, quadrature, shapes
 
-shape = shapes.TRIANGLE
-degree = 1
+shape = shapes.TETRAHEDRON
+degree = 0
 
 Uel = Nedelec.Nedelec( shape , degree )
+
 U = Uel.function_space()
 
 pts = shapes.make_points( shape , 1 , 2 , 3 )
