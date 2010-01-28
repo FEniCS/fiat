c6fed79b772c62a8d639599ac4daac37ac7cd7c2
Anders Logg <logg@simula.no>
2010-01-28 22:58:01 +0100

    Remove old FIAT directory

--- FIAT/__init__.py
+++
@@ -1,3 +0,0 @@
-"""FInite element Automatic Tabulator -- supports constructing and 
-evaluating arbitrary order Lagrange and many other elements.
-Simplices in one, two, and three dimensions are supported."""
