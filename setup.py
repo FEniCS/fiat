97e4d7ebc2916dc96372cee0efd0f5b3466f9366
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-12-03 18:19:57 +0000

    Remove redundant backslashes from setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name="FIAT", \
-      version="1.1.0+", \
-      description="FInite element Automatic Tabulator", \
-      author="Robert C. Kirby", \
-      author_email="robert.c.kirby@gmail.com", \
-      url="http://www.math.ttu.edu/~kirby", \
-      license="LGPL v3 or later", \
+setup(name="FIAT",
+      version="1.1.0+",
+      description="FInite element Automatic Tabulator",
+      author="Robert C. Kirby",
+      author_email="robert.c.kirby@gmail.com",
+      url="http://www.math.ttu.edu/~kirby",
+      license="LGPL v3 or later",
       packages=['FIAT'])
