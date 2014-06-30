a8c3c17170643c5989e648bd11ce8134656ccc00
Nico Schlömer <nico.schloemer@gmail.com>
2014-06-30 11:22:22 +0200

    small test cleanups

--- test/test.py
+++ test/test.py
@@ -16,7 +16,7 @@
 # along with FIAT. If not, see <http://www.gnu.org/licenses/>.
 #
 # First added:  2010-01-31
-# Last changed: 2014-06-23
+# Last changed: 2014-06-30
 
 import nose
 
@@ -26,82 +26,29 @@ import numpy
 from FIAT import supported_elements, make_quadrature, ufc_simplex, \
     newdubiner, expansions, reference_element
 
-# Combinations of (family, dim, degree) to test
-test_cases = (
-    ("Lagrange", 2, 1),
-    ("Lagrange", 2, 2),
-    ("Lagrange", 2, 3),
-    ("Lagrange", 3, 1),
-    ("Lagrange", 3, 2),
-    ("Lagrange", 3, 3),
-    ("Discontinuous Lagrange", 2, 0),
-    ("Discontinuous Lagrange", 2, 1),
-    ("Discontinuous Lagrange", 2, 2),
-    ("Discontinuous Lagrange", 3, 0),
-    ("Discontinuous Lagrange", 3, 1),
-    ("Discontinuous Lagrange", 3, 2),
-    ("Brezzi-Douglas-Marini", 2, 1),
-    ("Brezzi-Douglas-Marini", 2, 2),
-    ("Brezzi-Douglas-Marini", 2, 3),
-    ("Brezzi-Douglas-Marini", 3, 1),
-    ("Brezzi-Douglas-Marini", 3, 2),
-    ("Brezzi-Douglas-Marini", 3, 3),
-    ("Brezzi-Douglas-Fortin-Marini", 2, 2),
-    ("Raviart-Thomas", 2, 1),
-    ("Raviart-Thomas", 2, 2),
-    ("Raviart-Thomas", 2, 3),
-    ("Raviart-Thomas", 3, 1),
-    ("Raviart-Thomas", 3, 2),
-    ("Raviart-Thomas", 3, 3),
-    ("Discontinuous Raviart-Thomas", 2, 1),
-    ("Discontinuous Raviart-Thomas", 2, 2),
-    ("Discontinuous Raviart-Thomas", 2, 3),
-    ("Discontinuous Raviart-Thomas", 3, 1),
-    ("Discontinuous Raviart-Thomas", 3, 2),
-    ("Discontinuous Raviart-Thomas", 3, 3),
-    ("Nedelec 1st kind H(curl)", 2, 1),
-    ("Nedelec 1st kind H(curl)", 2, 2),
-    ("Nedelec 1st kind H(curl)", 2, 3),
-    ("Nedelec 1st kind H(curl)", 3, 1),
-    ("Nedelec 1st kind H(curl)", 3, 2),
-    ("Nedelec 1st kind H(curl)", 3, 3),
-    ("Nedelec 2nd kind H(curl)", 2, 1),
-    ("Nedelec 2nd kind H(curl)", 2, 2),
-    ("Nedelec 2nd kind H(curl)", 2, 3),
-    ("Nedelec 2nd kind H(curl)", 3, 1),
-    ("Nedelec 2nd kind H(curl)", 3, 2),
-    ("Nedelec 2nd kind H(curl)", 3, 3),
-    ("Crouzeix-Raviart", 2, 1),
-    ("Crouzeix-Raviart", 3, 1)
-    )
-
 # Parameters
-num_points = 3
-max_derivative = 3
 tolerance = 1e-8
 
 
 def test_expansions():
-    def _create_expansions_data():
+    def create_data():
         E = reference_element.DefaultTriangle()
         k = 3
         pts = E.make_lattice(k)
         Phis = expansions.get_expansion_set(E)
-
         phis = Phis.tabulate(k, pts)
         dphis = Phis.tabulate_derivatives(k, pts)
-
         return phis, dphis
 
     # Try reading reference values
     try:
         reference = pickle.load(open("reference-expansions.pickle", "r"))
     except IOError:
-        reference = _create_expansions_data()
+        reference = create_data()
         # Store the data for the future
         pickle.dump(reference, open("reference-expansions.pickle", "w"))
 
-    table_phi, table_dphi = _create_expansions_data()
+    table_phi, table_dphi = create_data()
     reference_table_phi, reference_table_dphi = reference
 
     # Test raw point data
@@ -121,7 +68,7 @@ def test_expansions():
 
 
 def test_expansions_jet():
-    def _create_expansions_jet_data():
+    def create_data():
         latticeK = 2
         n = 1
         order = 2
@@ -134,7 +81,7 @@ def test_expansions_jet():
         reference_jet = \
             pickle.load(open("reference-expansions-jet.pickle", "r"))
     except IOError:
-        reference_jet = _create_expansions_jet_data()
+        reference_jet = create_data()
         # Store the data for the future
         pickle.dump(
             reference_jet,
@@ -142,7 +89,7 @@ def test_expansions_jet():
             )
 
     # Test jet data
-    data = _create_expansions_jet_data()
+    data = create_data()
     reference_data = reference_jet
     for datum, reference_datum in zip(data, reference_data):
         diff = numpy.array(datum) - numpy.array(reference_datum)
@@ -152,7 +99,7 @@ def test_expansions_jet():
 
 
 def test_newdubiner():
-    def _create_newdubiner_data():
+    def create_data():
         latticeK = 2
         D = 3
         pts = newdubiner.make_tetrahedron_lattice(latticeK, float)
@@ -162,12 +109,12 @@ def test_newdubiner():
     try:
         reference = pickle.load(open("reference-newdubiner.pickle", "r"))
     except IOError:
-        reference = _create_newdubiner_data()
+        reference = create_data()
         # Store the data for the future
         pickle.dump(reference, open("reference-newdubiner.pickle", "w"))
 
     # Actually perform the test
-    table = _create_newdubiner_data()
+    table = create_data()
 
     for data, reference_data in zip(table, reference):
         for point, reference_point in zip(data, reference_data):
@@ -178,7 +125,7 @@ def test_newdubiner():
 
 
 def test_newdubiner_jet():
-    def _create_newdubiner_jet_data():
+    def create_data():
         latticeK = 2
         D = 3
         n = 1
@@ -190,14 +137,14 @@ def test_newdubiner_jet():
         reference_jet = \
             pickle.load(open("reference-newdubiner-jet.pickle", "r"))
     except IOError:
-        reference_jet = _create_newdubiner_jet_data()
+        reference_jet = create_data()
         # Store the data for the future
         pickle.dump(
             reference_jet,
             open("reference-newdubiner-jet.pickle", "w")
             )
 
-    table_jet = _create_newdubiner_jet_data()
+    table_jet = create_data()
     for datum, reference_datum in zip(table_jet, reference_jet):
         for entry, reference_entry in zip(datum, reference_datum):
             for k in range(3):
@@ -208,7 +155,58 @@ def test_newdubiner_jet():
 
 
 def test_quadrature():
-    def _create_data(family, dim, degree):
+    num_points = 3
+    max_derivative = 3
+    # Combinations of (family, dim, degree) to test
+    test_cases = (
+        ("Lagrange", 2, 1),
+        ("Lagrange", 2, 2),
+        ("Lagrange", 2, 3),
+        ("Lagrange", 3, 1),
+        ("Lagrange", 3, 2),
+        ("Lagrange", 3, 3),
+        ("Discontinuous Lagrange", 2, 0),
+        ("Discontinuous Lagrange", 2, 1),
+        ("Discontinuous Lagrange", 2, 2),
+        ("Discontinuous Lagrange", 3, 0),
+        ("Discontinuous Lagrange", 3, 1),
+        ("Discontinuous Lagrange", 3, 2),
+        ("Brezzi-Douglas-Marini", 2, 1),
+        ("Brezzi-Douglas-Marini", 2, 2),
+        ("Brezzi-Douglas-Marini", 2, 3),
+        ("Brezzi-Douglas-Marini", 3, 1),
+        ("Brezzi-Douglas-Marini", 3, 2),
+        ("Brezzi-Douglas-Marini", 3, 3),
+        ("Brezzi-Douglas-Fortin-Marini", 2, 2),
+        ("Raviart-Thomas", 2, 1),
+        ("Raviart-Thomas", 2, 2),
+        ("Raviart-Thomas", 2, 3),
+        ("Raviart-Thomas", 3, 1),
+        ("Raviart-Thomas", 3, 2),
+        ("Raviart-Thomas", 3, 3),
+        ("Discontinuous Raviart-Thomas", 2, 1),
+        ("Discontinuous Raviart-Thomas", 2, 2),
+        ("Discontinuous Raviart-Thomas", 2, 3),
+        ("Discontinuous Raviart-Thomas", 3, 1),
+        ("Discontinuous Raviart-Thomas", 3, 2),
+        ("Discontinuous Raviart-Thomas", 3, 3),
+        ("Nedelec 1st kind H(curl)", 2, 1),
+        ("Nedelec 1st kind H(curl)", 2, 2),
+        ("Nedelec 1st kind H(curl)", 2, 3),
+        ("Nedelec 1st kind H(curl)", 3, 1),
+        ("Nedelec 1st kind H(curl)", 3, 2),
+        ("Nedelec 1st kind H(curl)", 3, 3),
+        ("Nedelec 2nd kind H(curl)", 2, 1),
+        ("Nedelec 2nd kind H(curl)", 2, 2),
+        ("Nedelec 2nd kind H(curl)", 2, 3),
+        ("Nedelec 2nd kind H(curl)", 3, 1),
+        ("Nedelec 2nd kind H(curl)", 3, 2),
+        ("Nedelec 2nd kind H(curl)", 3, 3),
+        ("Crouzeix-Raviart", 2, 1),
+        ("Crouzeix-Raviart", 3, 1)
+        )
+
+    def create_data(family, dim, degree):
         '''Create the reference data.
         '''
         # Get domain and element class
@@ -226,7 +224,7 @@ def test_quadrature():
     def _perform_test(family, dim, degree, reference_table):
         '''Test against reference data.
         '''
-        table = _create_data(family, dim, degree)
+        table = create_data(family, dim, degree)
         # Check against reference
         for dtuple in reference_table:
             assert dtuple in table
@@ -242,7 +240,7 @@ def test_quadrature():
         reference = {}
         for test_case in test_cases:
             family, dim, degree = test_case
-            reference[test_case] = _create_data(family, dim, degree)
+            reference[test_case] = create_data(family, dim, degree)
         # Store the data for the future
         pickle.dump(reference, open("reference.pickle", "w"))
 
