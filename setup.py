67ccdccead35c11411fea2814aa204b5bf07d608
Matthias Liertzer <matthias@liertzer.at>
2015-02-06 14:57:05 +0100

    Bump version to 1.6.0dev to be PEP 440 conform

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ if sys.version_info < (2, 7):
     print("Python 2.7 or higher required, please upgrade.")
     sys.exit(1)
 
-setup(name="FIAT", \
-      version="1.5.0+", \
-      description="FInite element Automatic Tabulator", \
-      author="Robert C. Kirby", \
-      author_email="robert.c.kirby@gmail.com", \
-      url="http://www.math.ttu.edu/~kirby", \
-      license="LGPL v3 or later", \
+from FIAT import __version__ as version
+
+setup(name="FIAT",
+      version=version,
+      description="FInite element Automatic Tabulator",
+      author="Robert C. Kirby",
+      author_email="robert.c.kirby@gmail.com",
+      url="http://www.math.ttu.edu/~kirby",
+      license="LGPL v3 or later",
       packages=['FIAT'])
