b6208b628f6064e3c1161e1b4f462bc2ff37a1ab
Miklos Homolya <m.homolya14@imperial.ac.uk>
2016-07-21 12:19:35 +0100

    test create_quadrature for accuracy

--- test/unit/test_quadrature.py
+++ test/unit/test_quadrature.py
@@ -18,15 +18,107 @@
 # Written by David A. Ham (david.ham@imperial.ac.uk), 2015
 
 from __future__ import absolute_import
+from __future__ import division
 
+import numpy
 import pytest
 import FIAT
-import numpy
+from FIAT.reference_element import UFCInterval, UFCTriangle, UFCTetrahedron
+from FIAT.reference_element import FiredrakeQuadrilateral, TensorProductCell
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def interval():
-    return FIAT.reference_element.UFCInterval()
+    return UFCInterval()
+
+
+@pytest.fixture(scope='module')
+def triangle():
+    return UFCTriangle()
+
+
+@pytest.fixture(scope='module')
+def tetrahedron():
+    return UFCTetrahedron()
+
+
+@pytest.fixture(scope='module')
+def quadrilateral():
+    return FiredrakeQuadrilateral()
+
+
+@pytest.fixture(scope='module')
+def extr_interval():
+    """Extruded interval = interval x interval"""
+    return TensorProductCell(UFCInterval(), UFCInterval())
+
+
+@pytest.fixture(scope='module')
+def extr_triangle():
+    """Extruded triangle = triangle x interval"""
+    return TensorProductCell(UFCTriangle(), UFCInterval())
+
+
+@pytest.fixture(scope='module')
+def extr_quadrilateral():
+    """Extruded quadrilateral = quadrilateral x interval"""
+    return TensorProductCell(FiredrakeQuadrilateral(), UFCInterval())
+
+
+@pytest.mark.parametrize("scheme", ["canonical", "default"])
+@pytest.mark.parametrize("degree", range(8))
+def test_create_quadrature_interval(interval, degree, scheme):
+    q = FIAT.create_quadrature(interval, degree, scheme)
+    assert numpy.allclose(q.integrate(lambda x: x[0]**degree), 1/(degree + 1))
+
+
+@pytest.mark.parametrize("scheme", ["canonical", "default"])
+@pytest.mark.parametrize("degree", range(8))
+def test_create_quadrature_triangle(triangle, degree, scheme):
+    q = FIAT.create_quadrature(triangle, degree, scheme)
+    assert numpy.allclose(q.integrate(lambda x: sum(x)**degree), 1/(degree + 2))
+
+
+@pytest.mark.parametrize("scheme", ["canonical", "default"])
+@pytest.mark.parametrize("degree", range(8))
+def test_create_quadrature_tetrahedron(tetrahedron, degree, scheme):
+    q = FIAT.create_quadrature(tetrahedron, degree, scheme)
+    assert numpy.allclose(q.integrate(lambda x: sum(x)**degree), 1/(2*degree + 6))
+
+
+@pytest.mark.parametrize("scheme", ["canonical", "default"])
+@pytest.mark.parametrize("extrdeg", range(4))
+@pytest.mark.parametrize("basedeg", range(5))
+def test_create_quadrature_extr_interval(extr_interval, basedeg, extrdeg, scheme):
+    q = FIAT.create_quadrature(extr_interval, (basedeg, extrdeg), scheme)
+    assert numpy.allclose(q.integrate(lambda (x, y): x**basedeg * y**extrdeg),
+                          1/(basedeg + 1) * 1/(extrdeg + 1))
+
+
+@pytest.mark.parametrize("scheme", ["canonical", "default"])
+@pytest.mark.parametrize("extrdeg", range(4))
+@pytest.mark.parametrize("basedeg", range(5))
+def test_create_quadrature_extr_triangle(extr_triangle, basedeg, extrdeg, scheme):
+    q = FIAT.create_quadrature(extr_triangle, (basedeg, extrdeg), scheme)
+    assert numpy.allclose(q.integrate(lambda (x, y, z): (x + y)**basedeg * z**extrdeg),
+                          1/(basedeg + 2) * 1/(extrdeg + 1))
+
+
+@pytest.mark.parametrize("scheme", ["canonical", "default"])
+@pytest.mark.parametrize("degree", range(8))
+def test_create_quadrature_quadrilateral(quadrilateral, degree, scheme):
+    q = FIAT.create_quadrature(quadrilateral, degree, scheme)
+    assert numpy.allclose(q.integrate(lambda x: sum(x)**degree),
+                          (2**(degree + 2) - 2) / ((degree + 1)*(degree + 2)))
+
+
+@pytest.mark.parametrize("scheme", ["canonical", "default"])
+@pytest.mark.parametrize("extrdeg", range(4))
+@pytest.mark.parametrize("basedeg", range(5))
+def test_create_quadrature_extr_quadrilateral(extr_quadrilateral, basedeg, extrdeg, scheme):
+    q = FIAT.create_quadrature(extr_quadrilateral, (basedeg, extrdeg), scheme)
+    assert numpy.allclose(q.integrate(lambda (x, y, z): (x + y)**basedeg * z**extrdeg),
+                          (2**(basedeg + 2) - 2) / ((basedeg + 1)*(basedeg + 2)) * 1/(extrdeg + 1))
 
 
 @pytest.mark.parametrize(("points, degree"), ((p, d)
