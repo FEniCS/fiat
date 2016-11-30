b0fc20f5f733eaf94e2bc961dc69e338496d83d3
Johannes Ring <johannr@simula.no>
2016-11-30 10:22:14 +0100

    Update version number to 2017.1.0.dev0

--- FIAT/__init__.py
+++ FIAT/__init__.py
@@ -39,7 +39,7 @@ from FIAT.quadrature_schemes import create_quadrature     # noqa: F401
 from FIAT.reference_element import ufc_cell, ufc_simplex  # noqa: F401
 from FIAT.hdivcurl import Hdiv, Hcurl                     # noqa: F401
 
-__version__ = "2016.2.0"
+__version__ = "2017.1.0.dev0"
 
 # List of supported elements and mapping to element classes
 supported_elements = {"Argyris": Argyris,
