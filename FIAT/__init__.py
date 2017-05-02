dd2582b1a73dfd61548a070364852e93554dfc8f
Johannes Ring <johannr@simula.no>
2017-05-02 13:55:52 +0200

    Simplify version string

--- FIAT/__init__.py
+++ FIAT/__init__.py
@@ -4,6 +4,8 @@ Simplices in one, two, and three dimensions are supported."""
 
 from __future__ import absolute_import, print_function, division
 
+import pkg_resources
+
 # Import finite element classes
 from FIAT.finite_element import FiniteElement, CiarletElement  # noqa: F401
 from FIAT.argyris import Argyris
@@ -39,7 +41,7 @@ from FIAT.quadrature_schemes import create_quadrature     # noqa: F401
 from FIAT.reference_element import ufc_cell, ufc_simplex  # noqa: F401
 from FIAT.hdivcurl import Hdiv, Hcurl                     # noqa: F401
 
-__version__ = "2017.1.0.dev0"
+__version__ = pkg_resources.get_distribution("dijitso").version
 
 # List of supported elements and mapping to element classes
 supported_elements = {"Argyris": Argyris,
