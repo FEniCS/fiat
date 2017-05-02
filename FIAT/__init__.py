97821cbd10a18008f61c0e6ece4de20bfc78732e
Johannes Ring <johannr@simula.no>
2017-05-02 14:10:44 +0200

    Fix typo

--- FIAT/__init__.py
+++ FIAT/__init__.py
@@ -41,7 +41,7 @@ from FIAT.quadrature_schemes import create_quadrature     # noqa: F401
 from FIAT.reference_element import ufc_cell, ufc_simplex  # noqa: F401
 from FIAT.hdivcurl import Hdiv, Hcurl                     # noqa: F401
 
-__version__ = pkg_resources.get_distribution("dijitso").version
+__version__ = pkg_resources.get_distribution("FIAT").version
 
 # List of supported elements and mapping to element classes
 supported_elements = {"Argyris": Argyris,
