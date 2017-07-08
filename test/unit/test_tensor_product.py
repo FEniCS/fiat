93a4f95727ea34b903e4c181edda05f40e15ff43
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2017-07-08 15:16:56 +0300

    Added test case to compare hex tpe with flattened element

--- test/unit/test_tensor_product.py
+++ test/unit/test_tensor_product.py
@@ -522,14 +522,14 @@ def test_TFE_2Dx1D_vector_quad_hcurl():
         assert tab[dd][7][2][0] == 0.0
 
 
-def test_flattened_against_TPE():
+def test_flattened_against_tpe_quad():
     T = UFCInterval()
     P1 = Lagrange(T, 1)
-    tpe_element = TensorProductElement(P1, P1)
-    flattened_element = FlattenToQuadHex(tpe_element)
-    assert tpe_element.value_shape() == ()
-    tpe_tab = tpe_element.tabulate(1, [(0.1, 0.2)])
-    flattened_tab = flattened_element.tabulate(1, [(0.1, 0.2)])
+    tpe_quad = TensorProductElement(P1, P1)
+    flattened_quad = FlattenToQuadHex(tpe_quad)
+    assert tpe_quad.value_shape() == ()
+    tpe_tab = tpe_quad.tabulate(1, [(0.1, 0.2)])
+    flattened_tab = flattened_quad.tabulate(1, [(0.1, 0.2)])
 
     for da, db in [[(0,), (0,)], [(1,), (0,)], [(0,), (1,)]]:
         dc = da + db
@@ -537,6 +537,29 @@ def test_flattened_against_TPE():
         assert np.isclose(tpe_tab[dc][1][0], flattened_tab[dc][1][0])
         assert np.isclose(tpe_tab[dc][2][0], flattened_tab[dc][2][0])
         assert np.isclose(tpe_tab[dc][3][0], flattened_tab[dc][3][0])
+
+
+def test_flattened_against_tpe_hex():
+    T = UFCInterval()
+    P1 = Lagrange(T, 1)
+    tpe_quad = TensorProductElement(P1, P1)
+    tpe_hex = TensorProductElement(tpe_quad, P1)
+    flattened_quad = FlattenToQuadHex(tpe_quad)
+    flattened_hex = FlattenToQuadHex(TensorProductElement(flattened_quad, P1))
+    assert tpe_quad.value_shape() == ()
+    tpe_tab = tpe_hex.tabulate(1, [(0.1, 0.2, 0.3)])
+    flattened_tab = flattened_hex.tabulate(1, [(0.1, 0.2, 0.3)])
+
+    for da, db, dc in [[(0,), (0,), (0,)], [(1,), (0,), (0,)], [(0,), (1,), (0,)], [(0,), (0,), (1,)]]:
+        dd = da + db + dc
+        assert np.isclose(tpe_tab[dd][0][0], flattened_tab[dd][0][0])
+        assert np.isclose(tpe_tab[dd][1][0], flattened_tab[dd][1][0])
+        assert np.isclose(tpe_tab[dd][2][0], flattened_tab[dd][2][0])
+        assert np.isclose(tpe_tab[dd][3][0], flattened_tab[dd][3][0])
+        assert np.isclose(tpe_tab[dd][4][0], flattened_tab[dd][4][0])
+        assert np.isclose(tpe_tab[dd][5][0], flattened_tab[dd][5][0])
+        assert np.isclose(tpe_tab[dd][6][0], flattened_tab[dd][6][0])
+        assert np.isclose(tpe_tab[dd][7][0], flattened_tab[dd][7][0])
 
 
 if __name__ == '__main__':
