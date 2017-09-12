0bde0cbe0301f0aff911e034ef76b6d6f4f512bf
Jack S. Hale <mail@jackhale.co.uk>
2017-09-12 11:20:20 +0200

    PyPI name change.

--- FIAT/__init__.py
+++ FIAT/__init__.py
@@ -43,7 +43,7 @@ from FIAT.quadrature_schemes import create_quadrature     # noqa: F401
 from FIAT.reference_element import ufc_cell, ufc_simplex  # noqa: F401
 from FIAT.hdivcurl import Hdiv, Hcurl                     # noqa: F401
 
-__version__ = pkg_resources.get_distribution("FIAT").version
+__version__ = pkg_resources.get_distribution("fenics-fiat").version
 
 # List of supported elements and mapping to element classes
 supported_elements = {"Argyris": Argyris,
