812395ae3f7daa1c2b1b374ddf4d0794ba07393b
Lawrence Mitchell <lawrence@wence.uk>
2018-10-03 22:54:35 +0100

    Remove reliance on numpy.matrix
    Squashes more deprecation warnings.

--- FIAT/nedelec_second_kind.py
+++ FIAT/nedelec_second_kind.py
@@ -160,11 +160,11 @@ class NedelecSecondKindDual(DualSet):
 
             # Map Phis -> phis (reference values to physical values)
             J = Q_face.jacobian()
-            scale = 1.0 / numpy.sqrt(numpy.linalg.det(J.transpose() * J))
+            scale = 1.0 / numpy.sqrt(numpy.linalg.det(numpy.dot(J.T, J)))
             phis = numpy.ndarray((d, num_quad_points))
             for i in range(num_rts):
                 for q in range(num_quad_points):
-                    phi_i_q = scale * J * numpy.matrix(Phis[i, :, q]).transpose()
+                    phi_i_q = scale * numpy.dot(J, Phis[numpy.newaxis, i, :, q].T)
                     for j in range(d):
                         phis[j, q] = phi_i_q[j]
 
