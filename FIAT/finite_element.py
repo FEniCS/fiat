55fea3ded6ec52bf349d519e4eebae38e7a972f3
Cyrus Cheng <cyrus.cheng15@imperial.ac.uk>
2019-01-28 14:37:57 +0000

    Add ref_el as optional argument to CiarletElement.

--- FIAT/finite_element.py
+++ FIAT/finite_element.py
@@ -120,8 +120,8 @@ class CiarletElement(FiniteElement):
     basis generated from polynomials encoded in a `PolynomialSet`.
     """
 
-    def __init__(self, poly_set, dual, order, formdegree=None, mapping="affine"):
-        ref_el = poly_set.get_reference_element()
+    def __init__(self, poly_set, dual, order, ref_el=None, formdegree=None, mapping="affine"):
+        ref_el = ref_el or poly_set.get_reference_element()
         super(CiarletElement, self).__init__(ref_el, dual, order, formdegree, mapping)
 
         # build generalized Vandermonde matrix
