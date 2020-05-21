97a7bf4f2caf9b2c83cb629f1fb8bb2f0c50afef
Rob Kirby <robert.c.kirby@gmail.com>
2020-05-21 09:55:27 -0500

    work on doc strings

--- FIAT/functional.py
+++ FIAT/functional.py
@@ -67,11 +67,29 @@ def index_iterator(shp):
 # and gives a speed win there as well.
 
 class Functional(object):
-    """Class implementing an abstract functional.
-    All functionals are discrete in the sense that
-    the are written as a weighted sum of (components of) their
-    argument evaluated at particular points."""
+    """Abstract class representing a linear functional.
+    All FIAT functionals are discrete in the sense that
+    they are written as a weighted sum of (derivatives of components of) their
+    argument evaluated at particular points.
 
+    :arg ref_el: a :class:`Cell`
+    :arg target_shape: a tuple indicating the value shape of functions on
+         the functional operates (e.g. if the function eats 2-vectors
+         then target_shape is (2,) and if it eats scalars then
+         target_shape is ()
+    :arg pt_dict: A dict mapping points to lists of information about
+         how the functional is evaluated.  Each entry in the list takes
+         the form of a tuple (wt, comp) so that (at least if the 
+         deriv_dict argument is empty), the functional takes the form
+         :math:`\ell(f) = \sum_{q=1}^{N_q} \sum_{k=1}^{K_q} w^q_k f_{c_k}(x_q)`
+         where :math:`f_{c_k}` indicates a particular vector or tensor component
+    :arg deriv_dict: A dict that is similar to `pt_dict`, although the entries
+         of each list are tuples (wt, alpha, comp) with alpha a tuple
+         of nonnegative integers corresponding to the order of partial
+         differentiation in each spatial direction.
+    :arg functional_type: a string labeling the kind of functional
+         this is.
+    """
     def __init__(self, ref_el, target_shape, pt_dict, deriv_dict,
                  functional_type):
         self.ref_el = ref_el
@@ -114,23 +132,40 @@ class Functional(object):
         return self.functional_type
 
     def to_riesz(self, poly_set):
-        """Constructs an array representation of the functional over
-        the base of the given polynomial_set so that f(phi) for any
-        phi in poly_set is given by a contraction of phi's coefficients
-        in its expansion set with the array representation.
+        """Constructs an array representation of the functional so
+        that the functional may be applied to a function expressed in 
+        in terms of the expansion set underlying  `poly_set` by means
+        of contracting coefficients.
+
+        That is, `poly_set` will have members all expressed in the
+        form :math:`p = \sum_{i} \alpha^i \phi_i`
+        where :math:`\{\phi_i\}_{i}` is some orthonormal expansion set
+        and :math:`\alpha^i` are coefficients.  Note: the orthonormal
+        expansion set is always scalar-valued but if the members of
+        `poly_set` are vector or tensor valued the :math:`\alpha^i`
+        will be scalars or vectors.
+
+        This function constructs a tensor :math:`R` such that the
+        contraction of :math:`R` with the array of coefficients
+        :math:`\alpha` produces the effect of :math:`\ell(f)`
+
+        In the case of scalar-value functions, :math:`R` is just a
+        vector of the same length as the expansion set, and 
+        :math:`R_i = \ell(\phi_i)`.  For vector-valued spaces,
+        :math:`R_{ij}` will be :math:`\ell(e^i \phi_j)` where 
+        :math:`e^i` is the canonical unit vector nonzero only in one
+        entry :math:`i`.
         """
         es = poly_set.get_expansion_set()
         ed = poly_set.get_embedded_degree()
+        nexp = es.get_num_members(ed)
+
         pt_dict = self.get_point_dict()
 
         pts = list(pt_dict.keys())
-
-        # bfs is matrix that is pdim rows by num_pts cols
-        # where pdim is the polynomial dimension
-
-        bfs = es.tabulate(ed, pts)
         npts = len(pts)
-        nexp = es.get_num_members(ed)
+        
+        bfs = es.tabulate(ed, pts)
         result = numpy.zeros(poly_set.coeffs.shape[1:], "d")
 
         # loop over points
@@ -151,7 +186,6 @@ class Functional(object):
             es_foo = polynomial_set.ONPolynomialSet(self.ref_el, ed)
             dpts = list(dpt_dict.keys())
 
-            # figure out the maximum order of derivative being taken
             dbfs = es_foo.tabulate(dpts, self.max_deriv_order)
 
             ndpts = len(dpts)
@@ -492,7 +526,6 @@ class IntegralMomentOfTensorDivergence(Functional):
         dpt_dict = OrderedDict()
 
         alphas = [tuple([1 if j == i else 0 for j in range(sd)]) for i in range(sd)]
-        print(alphas)
         for q, pt in enumerate(dpts):
             dpt_dict[tuple(pt)] = [(qwts[q]*f_at_qpts[i, q], alphas[j], (i, j)) for i in range(2) for j in range(2)]
 
