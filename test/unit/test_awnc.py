1163eeb777c5123ab659c48d62188f22aae08e17
Rob Kirby <robert.c.kirby@gmail.com>
2020-08-19 12:24:05 -0500

    move import in awnc test

--- test/unit/test_awnc.py
+++ test/unit/test_awnc.py
@@ -1,9 +1,7 @@
 import numpy as np
-
+from FIAT import ufc_simplex, ArnoldWintherNC, make_quadrature, expansions
 
 def test_dofs():
-    from FIAT import ufc_simplex, ArnoldWintherNC, make_quadrature, expansions
-
     line = ufc_simplex(1)
     T = ufc_simplex(2)
     T.vertices = np.random.rand(3, 2)
